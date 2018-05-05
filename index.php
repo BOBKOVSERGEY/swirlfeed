dacb46f1ab739c9043f1044bba30d95fd99aa8d4
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-05-05 11:41:28 +0300

    Вывод постов и лайков

--- index.php
+++ index.php
@@ -8,9 +8,22 @@ include __DIR__ . '/includes/header.php';
   <div class="container">
     <div class="row">
       <div class="col-md-3">
-        <div class="user-details__block">
-          <div class="user-details__main-img p-3">
-            <a href="#"><img src="<?php echo $user['profile_pic']?>" alt=""></a>
+        <div class="card">
+          <div class="card-body">
+            <div class="user-details__main-img p-3">
+              <a href="#"><img src="<?php echo $user['profile_pic']?>" alt=""></a>
+            </div>
+            <div>
+              <a href="#">
+                <?php echo $user['first_name'] . " " . $user['last_name'];?>
+              </a>
+            </div>
+            <div>
+              <?php echo "Постов: " . $user['num_posts']; ?>
+            </div>
+            <div>
+              <?php echo "Лайков: " . $user['num_likes']; ?>
+            </div>
           </div>
         </div>
       </div>
